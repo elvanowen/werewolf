8b54e7ceb2894c11af1e1488571f1f6d5aeeb59d
edwinwijaya94 <edwinwijaya94@yahoo.com>
2016-04-28 00:23:26 +0700

    update server:join,ready,startgame

--- src/TCPServer.java
+++ src/TCPServer.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class TCPServer extends Thread {
     private ServerSocket serverSocket;
-    private ArrayList<ClientSocket> clientSocketList;
+    private ArrayList<Client> clientList = new ArrayList<>();
 
     public TCPServer(){
         this(8888);
@@ -28,8 +28,9 @@ public class TCPServer extends Thread {
 
                 Socket _clientSocket = serverSocket.accept();
 
-                ClientSocket clientSocket = new ClientSocket(this, _clientSocket);
-                clientSocketList.add(clientSocket);
+                Client client = new Client(this, _clientSocket);
+                clientList.add(client); // potential players
+                
             } catch(SocketTimeoutException s) {
                 System.out.println("Socket timed out!");
                 break;
@@ -40,31 +41,61 @@ public class TCPServer extends Thread {
         }
     }
 
-    public void onMessageReceived(String message){
-
+    public void onMessageReceived(Client client, String message){
+        System.out.println("on: " + message); 
+        Server.onMessageReceived(client, message); // pass to server
     }
 
-    public void broadcast(String message){
-        for (ClientSocket clientSocket: clientSocketList) {
-            clientSocket.send(message);
+    public void broadcast(HashMap<String, TCPServer.Client>clientList ,String message){
+        
+    }
+
+    public void send(TCPServer.Client client, String message){
+        client.clientSocket.send(message);
+    }
+    
+    // client
+    public class Client{
+        int playerId;
+        int isAlive; // 0 or 1
+        String address;
+        int port;
+        String username;
+        String role;
+        String status; // "join", "ready"
+        ClientSocket clientSocket;
+        
+        //called when client create socket conn
+        public Client(TCPServer tcpServer, Socket socket){
+            clientSocket = new ClientSocket(tcpServer, socket,this);
+            
+            //set client address and port
+            this.address = socket.getInetAddress().toString();
+            this.port = socket.getPort();
+        }
+        
+        //called when client join game
+        void setClient(int playerId,String username, String role){
+            this.playerId = playerId;
+            this.isAlive = 1;
+            this.username = username;
+            this.role = role;
+            this.status = "join"; // set initial client status
         }
     }
-
-    public void send(ArrayList<ClientSocket> clientSocketList, String message){
-        for (ClientSocket clientSocket: clientSocketList) {
-            clientSocket.send(message);
-        }
-    }
-
+    
+    
     private class ClientSocket implements Runnable {
         private Socket clientSocket = null;
         private Thread thread;
         private TCPServer server;
-
-        public ClientSocket(TCPServer server, Socket clientSocket){
+        private Client client; //reference to client
+        
+        public ClientSocket(TCPServer server, Socket clientSocket, Client client){
             this.server = server;
             this.clientSocket = clientSocket;
-
+            this.client = client;
+            
             System.out.println("Just connected to " + this.clientSocket.getRemoteSocketAddress());
 
             thread = new Thread(this);
@@ -74,8 +105,12 @@ public class TCPServer extends Thread {
         public void run(){
             while (true){
                 try {
-                    DataInputStream in = new DataInputStream(this.clientSocket.getInputStream());
-                    this.server.onMessageReceived(in.readUTF());
+                    BufferedReader br = new BufferedReader(new InputStreamReader(this.clientSocket.getInputStream()));
+                    String inLine = null;
+                    while (((inLine = br.readLine()) != null) && (!(inLine.equals("")))) {
+                        this.server.onMessageReceived(this.client,inLine);
+                    }
+//                    System.out.println("join received");
                 } catch (Exception e) {
                     System.out.println(e.getMessage());
                 }
@@ -83,9 +118,11 @@ public class TCPServer extends Thread {
         }
 
         public void send(String message){
+            PrintStream output;
             try {
-                DataOutputStream out = new DataOutputStream(this.clientSocket.getOutputStream());
-                out.writeUTF(message);
+                output = new PrintStream(clientSocket.getOutputStream());
+                output.println(message);
+                output.flush();
             } catch (Exception e) {
                 System.out.println(e.getMessage());
             }
